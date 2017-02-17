db827d55fa1ef4dcc87996dd75e364d83373a82c
Ghazamalghifari <ghozam.alghifari96@gmail.com>
2017-02-17 15:37:20 +0700

    menambahkan link di menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,8 @@
                     <li><a href="{{ url('/home') }}">Dashboard</a></li>
                      <li><a href="{{route('tugas.index')}}">Tugas</a></li>
                     <li><a href="{{ route('kategori.index') }}">Kategori</a></li>
+                    <li><a href="{{ route('lokasi.index') }}">Lokasi</a></li>
+                    <li><a href="{{ route('otoritas.index') }}">Otoritas</a></li>
                     @endif
 
                     </ul>
